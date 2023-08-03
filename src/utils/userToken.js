031df719198bda6e670577a9cc909a2bb6f9d8b7
symohdev21@gmail.com <symohdev21@gmail.com>
2023-08-03 06:33:26 -0700

    updating jwt key

--- src/utils/userToken.js
+++ src/utils/userToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const createToken = (id) => {
-    const jwtKey = process.env.JWT_SECRET_kEY;
+    const jwtKey = process.env.JWT_SECRET_KEY;
     return jwt.sign({ id }, jwtKey, { expiresIn: "3d" })
 }
 
